032e88ad7a6ccbfc09f3092a9f37fbbce6c4b8af
Carloslt <carloslt.proyectos@gmail.com>
2023-11-05 20:08:49 +0100

    add helper hbs

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -8,13 +8,13 @@ const Movie = require('../models/Movie.model')
 // Show Movies
 router.get('/movies', (req, res, next) => {
 
-    Movie
-        .find()
-        .sort({ title: 1 })
-        .then(moviesFromDB => {
-            res.render('movies/movies', { moviesFromDB })
-        })
-        .catch(err => console.log(err))
+  Movie
+    .find()
+    .sort({ title: 1 })
+    .then(moviesFromDB => {
+      res.render('movies/movies', { moviesFromDB })
+    })
+    .catch(err => console.log(err))
 
 })
 
@@ -22,96 +22,96 @@ router.get('/movies', (req, res, next) => {
 // Create movie (render)
 router.get('/movies/create', (req, res, next) => {
 
-    Celebrity
-        .find()
-        .select({ name: 1 })
-        .sort({ name: 1 })
-        .then(celebritiesFromDB => {
-            res.render('movies/new-movie', { celebritiesFromDB })
-        })
-        .catch(err => console.log(err))
+  Celebrity
+    .find()
+    .select({ name: 1 })
+    .sort({ name: 1 })
+    .then(celebritiesFromDB => {
+      res.render('movies/new-movie', { celebritiesFromDB })
+    })
+    .catch(err => console.log(err))
 
 })
 
 
 // Create movie (handler)
 router.post('/movies/create', (req, res, next) => {
-    const { title, genre, plot, cast } = req.body
+  const { title, genre, plot, cast } = req.body
 
-    Movie
-        .create({ title, genre, plot, cast })
-        .then(() => {
-            res.redirect('/movies')
-        })
-        .catch(err => console.log(err))
+  Movie
+    .create({ title, genre, plot, cast })
+    .then(() => {
+      res.redirect('/movies')
+    })
+    .catch(err => console.log(err))
 
 })
 
 
 // Movies details (render)
 router.get('/movies/:id', (req, res, next) => {
-    const { id } = req.params
+  const { id } = req.params
 
-    Movie
-        .findById(id)
-        .populate('cast')
-        .then(movie => {
-            res.render('movies/movie-details', movie)
-        })
-        .catch(err => console.log(err))
+  Movie
+    .findById(id)
+    .populate('cast')
+    .then(movie => {
+      res.render('movies/movie-details', movie)
+    })
+    .catch(err => console.log(err))
 
 })
 
 
 //Edit Movie (render)
 router.get('/movies/:id/edit', (req, res, next) => {
-    const { id } = req.params
+  const { id } = req.params
 
-    Movie
-        .findById(id)
-        .populate('cast')
-        .then(movie => {
-            return movie
+  Movie
+    .findById(id)
+    .populate('cast')
+    .then(movie => {
+      return movie
+    })
+    .then(movie => {
+      Celebrity
+        .find()
+        .sort({ name: 1 })
+        .then(celebritiesFromDB => {
+          // res.send({ movie, celebritiesFromDB })
+          return res.render('movies/edit-movie', { movie, celebritiesFromDB })
         })
-        .then(movie => {
-            Celebrity
-                .find()
-                .sort({ name: 1 })
-                .then(celebritiesFromDB => {
-                    // res.send({ movie, celebritiesFromDB })
-                    return res.render('movies/edit-movie', { movie, celebritiesFromDB })
-                })
-        })
-        .catch(err => console.log(err))
+    })
+    .catch(err => console.log(err))
 
 })
 
 
 //Edit Movie (handler)
 router.post('/movies/:id/edit', (req, res, next) => {
-    const { id } = req.params
-    const { title, genre, plot, cast } = req.body
+  const { id } = req.params
+  const { title, genre, plot, cast } = req.body
 
-    Movie
-        .findByIdAndUpdate(id, { title, genre, plot, cast })
-        .then(() => {
-            res.redirect(`/movies/${id}`)
-        })
-        .catch(err => console.log(err))
+  Movie
+    .findByIdAndUpdate(id, { title, genre, plot, cast })
+    .then(() => {
+      res.redirect(`/movies/${id}`)
+    })
+    .catch(err => console.log(err))
 
 })
 
 
 //Delete Movie
 router.post('/movies/:id/delete', (req, res, next) => {
-    const { id } = req.params
+  const { id } = req.params
 
-    Movie
-        .findByIdAndDelete(id)
-        .then(() => {
-            res.redirect('/movies')
-        })
-        .catch(err => console.log(err))
+  Movie
+    .findByIdAndDelete(id)
+    .then(() => {
+      res.redirect('/movies')
+    })
+    .catch(err => console.log(err))
 
 })
 
